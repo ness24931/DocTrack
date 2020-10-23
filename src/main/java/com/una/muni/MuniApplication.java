76ca840191b824d673c7f66a9b19402274a3ec24
Nestor Pasos <ness_199348@hotmail.com>
2020-10-23 12:23:46 -0600

    pruebas finalizadas de pull y rebase

--- src/main/java/com/una/muni/MuniApplication.java
+++ src/main/java/com/una/muni/MuniApplication.java
@@ -3,12 +3,10 @@ package com.una.muni;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/*prueba 3 para git pull y git rebase*/
 @SpringBootApplication
 public class MuniApplication {
 
-	public static void main(String[] args) {
-		/*esta es una prueba*/
+	public static void main(String[] args) {		
 		SpringApplication.run(MuniApplication.class, args);
 	}
 

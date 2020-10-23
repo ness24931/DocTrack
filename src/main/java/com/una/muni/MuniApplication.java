8406ae5bf581542997b20266592907af815a3b22
Nestor Pasos <ness_199348@hotmail.com>
2020-10-23 11:31:27 -0600

    prueba pull --rebase

--- src/main/java/com/una/muni/MuniApplication.java
+++ src/main/java/com/una/muni/MuniApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MuniApplication {
 
 	public static void main(String[] args) {
+		/*esta es una prueba*/
 		SpringApplication.run(MuniApplication.class, args);
 	}
 
